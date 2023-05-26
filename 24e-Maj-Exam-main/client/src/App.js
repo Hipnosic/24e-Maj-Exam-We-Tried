011b32c5db031e54ad553544187c26516a0c0c5a
Daniel Rotaru <daniel_rotaru@hotmail.com>
2023-05-26 10:14:57 +0200

    Added user component

--- 24e-Maj-Exam-main/client/src/App.js
+++ 24e-Maj-Exam-main/client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Guest from "./components/guest";
 import Admin from './components/admin';
+import User from './components/user';
 
 function App() {
   return (
@@ -13,10 +14,14 @@ function App() {
               <li>
                 <Link to="/admin">Admin</Link>
               </li>
+              <li>
+                <Link to="/user">User</Link>
+              </li>
             </ul>
            <Routes>
                  <Route exact path='/' element={< Guest />}></Route>
                  <Route exact path='/admin' element={< Admin />}></Route>
+                 <Route ezact path='/user' element={< User />}></Route>
           </Routes>
     </BrowserRouter>
   );
---
+++ 24e-Maj-Exam-main/client/src/components/user.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = () => {
+    return (<p>Welcome, user</p>)
+}
+
+export default User;
