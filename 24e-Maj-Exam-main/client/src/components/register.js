27f7570ba8fea918727e15deca9a26aee48b62d2
Lucas Magnusson <lucasmyeet@gmail.com>
2023-05-29 09:32:20 +0200

    fixed register frontend

--- 24e-Maj-Exam-main/client/src/components/register.js
+++ 24e-Maj-Exam-main/client/src/components/register.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -13,8 +13,6 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Register = () => {
-
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
@@ -28,17 +26,20 @@ function Copyright(props) {
   );
 }
 
+// TODO remove, this demo shouldn't need to reset the theme.
+
 const defaultTheme = createTheme();
 
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
 
-      const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-          email: data.get('email'),
-          password: data.get('password'),
-        });
-      
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
@@ -130,6 +131,3 @@ const defaultTheme = createTheme();
     </ThemeProvider>
   );
 }
-}
-
-export default Register;
