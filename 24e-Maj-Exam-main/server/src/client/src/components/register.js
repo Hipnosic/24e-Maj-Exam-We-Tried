a4b39b4f557d5fcdccec661a90776a8023cd0ec4
Lucas Magnusson <lucasmyeet@gmail.com>
2023-05-29 11:20:37 +0200

    repathed authservice in register

--- 24e-Maj-Exam-main/server/src/client/src/components/register.js
+++ 24e-Maj-Exam-main/server/src/client/src/components/register.js
@@ -12,7 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import AuthService from '../../../server/src/service/authService';
+import AuthService from '../../../service/authService';
 
 function Copyright(props) {
   return (
