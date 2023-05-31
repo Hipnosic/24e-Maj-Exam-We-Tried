e232072cc901d14dd894dc69bf368f8b5fe4f026
Daniel Rotaru <daniel_rotaru@hotmail.com>
2023-05-31 15:29:52 +0200

    update

--- 24e-Maj-Exam-main/client/src/App.test.js
+++ 24e-Maj-Exam-main/client/src/App.test.js
@@ -1,11 +1,4 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+import { render, screen } from "@testing-library/react";
 
 test("1 + 1 = 2", () => {
   const a = 1;
