14c0e49bbf3eca7c74ca9cae532033e86b8ca73d
Daniel Rotaru <daniel_rotaru@hotmail.com>
2023-06-02 00:24:13 +0200

    update axius prob

---
+++ 24e-Maj-Exam-main/client/src/__tests__/BooksTable.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import BooksTable from "../components/books/BooksTable";
+
+describe("BooksTable", () => {
+  it("renders an array of books with title, author, and quantity", () => {
+    const booksData = [
+      { title: "Book 1", author: "Author 1", quantity: 3 },
+      { title: "Book 2", author: "Author 2", quantity: 5 },
+      { title: "Book 3", author: "Author 3", quantity: 2 },
+    ];
+
+    const { getByText } = render(<BooksTable booksData={booksData} />);
+
+    // Check if each book's title, author, and quantity are rendered
+    booksData.forEach((book) => {
+      expect(getByText(book.title)).toBeInTheDocument();
+      expect(getByText(book.author)).toBeInTheDocument();
+      expect(getByText(book.quantity.toString())).toBeInTheDocument();
+    });
+  });
+});
